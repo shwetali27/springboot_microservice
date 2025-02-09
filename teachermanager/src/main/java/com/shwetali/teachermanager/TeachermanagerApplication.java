107dff67008b69dfc6c766b91fad1bb4c871e47a
Shwetali Khambe <khambeshwetali@ymail.com>
2025-02-09 22:47:36 +0530

    First commit for springboot micoservice demo project

---
+++ teachermanager/src/main/java/com/shwetali/teachermanager/TeachermanagerApplication.java
@@ -0,0 +1,13 @@
+package com.shwetali.teachermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeachermanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeachermanagerApplication.class, args);
+	}
+
+}
